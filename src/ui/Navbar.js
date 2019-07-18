d2da851315f6ebaf492f69b5f1eb8f22f2631861
Marlon Batista <marlon.batista18@hotmail.com>
2019-07-18 18:02:11 -0300

    teste

--- src/ui/Navbar.js
+++ src/ui/Navbar.js
@@ -31,6 +31,7 @@ const NavBar = (props) =>{
         <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
       </li>
     </ul>
+    {/*Inclusão*/}
     <form className="form-inline my-2 my-lg-0">
       <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" ></input>
       <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
